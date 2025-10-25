f6f4c99832e21d20d8f527f66228bc46ffa7e223
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-25 14:13:40 +0530

    dormant css done

--- src/TrafficLights.jsx
+++ src/TrafficLights.jsx
@@ -1,50 +1,35 @@
-import React from 'react'
-import './trafficLights.css'
+import React from "react";
+import "./trafficLights.css";
 
 const TrafficLights = () => {
-//    const [isActive, setActive] = useState(true);
-   // const [light2, setLight2] = useState(false);
-   // const [light3, setLight3] = useState(false);
+  const style = {
+    backgroundColor: "gray",
+    height: "4rem",
+    width: "4rem",
+    margin: "auto",
+    borderRadius: "50%",
+  };
 
-//    const [currentLight, setCurrentLight] = useState("gray");
+  return (
+    <div className="app">
+      <div
+        className="container_box"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          backgroundColor: "black",
+          height: "30rem",
+          width: "10rem",
+          margin: "auto",
+        }}
+      >
+        <div className="container" style={style}></div>
+        <div className="container" style={style}></div>
+        <div className="container" style={style}></div>
+      </div>
+    </div>
+  );
+};
 
-   // const lightWorks = () => {
-   //   setInterval(() => {
-   //    \
-
-
-   const style = {
-     backgroundColor: "gray",
-     height: "4rem",
-     width:"4rem",
-     margin:"auto",
-     borderRadius:"50%"
-     
-
-
-
-   };
-  
-   return (
-     <div className="app">
-       <div
-         className="container_box"
-         style={{
-           display: "flex",
-           alignItems: "center",
-           flexDirection: "column",
-           backgroundColor: "black",
-           height: "30rem",
-           width: "10rem",
-           margin: "auto",
-         }}
-       >
-         <div className="container" style={style}></div>
-         <div className="container" style={style}></div>
-         <div className="container" style={style}></div>
-       </div>
-     </div>
-   );
-}
-
-export default TrafficLights
+export default TrafficLights;
