ebaf1e72bc3cca10c51c5938db1498be51652a7c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-25 12:40:02 +0530

    first

--- src/TrafficLights.jsx
+++ src/TrafficLights.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
 const TrafficLights = () => {
-  return (
-    <div>
-      <h2>Hello</h2>
-    </div>
-  )
+//    const [isActive, setActive] = useState(true);
+   // const [light2, setLight2] = useState(false);
+   // const [light3, setLight3] = useState(false);
+
+//    const [currentLight, setCurrentLight] = useState("gray");
+
+   // const lightWorks = () => {
+   //   setInterval(() => {
+   //    \
+
+  
+   return (
+     <div className="app">
+       <div className="container_box">
+         <div className="container" style={{ backgroundColor: "red" }}></div>
+         <div className="container"></div>
+         <div className="container"></div>
+       </div>
+     </div>
+   );
 }
 
 export default TrafficLights
