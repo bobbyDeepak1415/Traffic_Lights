3502b4d30e56be4a37b9107b798dfc6bc33edcbd
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-10-28 02:06:52 +0530

    dormant css done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 
 const Demo = () => {
+  const lights = ["red", "orange", "green"];
+
   return (
-    <div>
-    <div className="containers_Box"></div>
+    <div style={{ display: "flex" }}>
+      <div
+        className="containers_Box"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          height: "22rem",
+          width: "10rem",
+          backgroundColor: "black",
+        }}
+      >
+        {lights.map((light, index) => {
+          return (
+            <div
+            key={index}
+              className="container"
+              style={{
+                backgroundColor: "gray",
+                height: "5rem",
+                width: "5rem",
+                borderRadius: "50%",
+                margin:"auto"
+              }}
+            ></div>
+          );
+        })}
+      </div>
     </div>
   );
 };
