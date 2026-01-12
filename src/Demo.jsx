5d55641e2f4009b766351703eca617bcc93f5368
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-12 17:29:18 +0530

    debugging

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,51 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Demo = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const lights = ["red", "orange", "green"];
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prev) => prev + 1);
-    }, 1500);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const getColour = (index) => {
-    return currentIndex === index ? lights[index] : "gray";
-  };
-
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <div
-        className="containers_Box"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-          height: "22rem",
-          width: "10rem",
-          backgroundColor: "black",
-        }}
-      >
-        {lights.map((light, index) => {
-          return (
-            <div
-              key={index}
-              className="container"
-              style={{
-                backgroundColor: getColour(index),
-                height: "5rem",
-                width: "5rem",
-                borderRadius: "50%",
-                margin: "auto",
-              }}
-            ></div>
-          );
-        })}
-      </div>
+    <div>
+      <h2>Helllo</h2>
     </div>
   );
 };
