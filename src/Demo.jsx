064c860f202cff6aafa8ec3cddb5c802c70ea57f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-01 16:44:22 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,59 +1,7 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Demo = () => {
-  const [users, setUsers] = useState([]);
-  const [filter, setFilter] = useState("All");
-
-  const [statuses, setStatuses] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://rickandmortyapi.com/api/character",
-        );
-        const result = response.data.results;
-
-        const uniqueStatuses = Array.from(
-          new Set(result.map((user) => user.status)),
-        );
-
-        setStatuses(uniqueStatuses);
-        setUsers(result);
-      } catch (e) {
-        console.log("failed to fetch", e);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const filteredUsers =
-    filter === "All" ? users : users.filter((user) => user.status === filter);
-
-  return (
-    <div style={{ height: "100vh", backgroundColor: "slategray" }}>
-      <h2>Helllo</h2>
-      <select value={filter} onChange={(e) => setFilter(e.target.value)}>
-        {statuses.map((status, index) => {
-          return (
-            <option key={index} value={status}>
-              {status}
-            </option>
-          );
-        })}
-      </select>
-      {filteredUsers.map((user) => {
-        return (
-          <li key={user.id}>
-            {user.name}
-            <span>:--{user.status}</span>
-          </li>
-        );
-      })}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Demo;
